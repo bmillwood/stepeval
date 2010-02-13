90259dbd35e55720e94ed37d268eb757db6f5164
Ben Millwood <benmachine+git@mercenariesguild.net>
2010-02-13 18:38:14 +0000

    Improve the CGI script a little

--- src/Main.hs
+++ src/Main.hs
@@ -34,22 +34,36 @@ cliMain = do
 
 cgiMain :: String -> IO ()
 cgiMain qstr = do
- putStrLn . unlines $
+ let exp = case dropWhile (/= '=') qstr of
+      _ : v -> unescape v
+      "" -> ""
+ putStrLn . concat $
   ["Content-Type: text/html; charset=UTF-8\n\n",
-   "<html>\n<head><title>Step-by-step evaluator</title></head>",
-   "<body>\n<form method=\"get\" action=\"\">",
-   "<textarea rows=\"5\" cols=\"80\" name=\"expr\"></textarea><br>",
-   "<input type=\"submit\" value=\"Evaluate!\">",
-   "</form>\n<pre>"]
- myThreadId >>= forkIO . (threadDelay 500000 >>) . killThread
- case parseExp (unescape . tail . dropWhile (/= '=') $ qstr) of
-  ParseOk e -> printeval e `catch` (\e -> print (e :: ErrorCall))
+   "<html>\n<head>\n",
+   "<title>Step-by-step evaluator</title>\n",
+   "<style type=\"text/css\">\n",
+   "ol { white-space: pre; font-family: monospace }\n</style>\n",
+   "</head>\n",
+   "<body>\n<form method=\"get\" action=\"\">\n",
+   "<textarea rows=\"5\" cols=\"80\" name=\"expr\">",
+   exp,
+   "</textarea><br>\n",
+   "<input type=\"submit\" value=\"Evaluate!\">\n",
+   "</form>\n"]
+ myThreadId >>= forkIO . (threadDelay 250000 >>) . killThread
+ unless (null exp) $ case parseExp exp of
+  ParseOk e -> putStrLn (pp e) `catches`
+   [Handler $ \e -> print (e :: ErrorCall),
+    Handler $ \e -> const (putStrLn "Time limit expired!")
+     (e :: AsyncException)]
   ParseFailed _ _ -> putStrLn "Sorry, parsing failed."
- putStrLn "</pre>\n</body>\n</html>"
+ putStrLn "\n</body>\n</html>"
  where unescape ('+':cs) = ' ':unescape cs
        unescape ('%':a:b:cs) = case readHex [a, b] of
         [(x, "")] -> chr x : unescape cs
         _ -> error $ "Failed to parse percent escape: " ++ [a, b]
        unescape (c:cs) = c:unescape cs
        unescape [] = ""
+       pp e = "<ol>" ++ concatMap (("<li>" ++) . (++ "</li>\n") .
+        prettyPrint) (itereval e)
 

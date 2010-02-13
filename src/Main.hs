123e9781fd64161e4cbe6c1e65d7ebba2b65de2c
Ben Millwood <benmachine+git@mercenariesguild.net>
2010-02-13 15:55:30 +0000

    Convert CGI script to output HTML

--- src/Main.hs
+++ src/Main.hs
@@ -34,11 +34,18 @@ cliMain = do
 
 cgiMain :: String -> IO ()
 cgiMain qstr = do
- putStr "Content-Type: text/plain; charset=UTF-8\n\n"
+ putStrLn . unlines $
+  ["Content-Type: text/html; charset=UTF-8\n\n",
+   "<html>\n<head><title>Step-by-step evaluator</title></head>",
+   "<body>\n<form method=\"get\" action=\"\">",
+   "<textarea rows=\"5\" cols=\"80\" name=\"expr\"></textarea><br>",
+   "<input type=\"submit\" value=\"Evaluate!\">",
+   "</form>\n<pre>"]
  myThreadId >>= forkIO . (threadDelay 500000 >>) . killThread
  case parseExp (unescape . tail . dropWhile (/= '=') $ qstr) of
   ParseOk e -> printeval e `catch` (\e -> print (e :: ErrorCall))
   ParseFailed _ _ -> putStrLn "Sorry, parsing failed."
+ putStrLn "</pre>\n</body>\n</html>"
  where unescape ('+':cs) = ' ':unescape cs
        unescape ('%':a:b:cs) = case readHex [a, b] of
         [(x, "")] -> chr x : unescape cs
